e29d86c17f97b4b72ba71a5496eecd7177775c09
harishkumar0004 <harishkumar727622@gmail.com>
2026-07-10 18:49:02 +0530

    agv+180_navigation

--- agv_straight_0_180_test.py
+++ agv_straight_0_180_test.py
@@ -445,7 +445,7 @@ def corrected_map_heading_from_tag(map_heading, tag_heading):
     return corrected_heading
 
 
-def lateral_command_for_heading(x_corrected, active_heading):
+def lateral_command_for_heading(x_corrected, active_heading, tag_heading):
     h = normalize_angle(active_heading)
 
     if abs(normalize_angle(h - 0.0)) < 3.0:
